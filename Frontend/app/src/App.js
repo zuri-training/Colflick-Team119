fc0f7599eafa1c4b26e6169d67063fc7cd9ba75f
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-08-05 11:15:51 +0100

    FRONTEND: Installed react, tailwind css and generated layout for app

---
+++ Frontend/app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+};
+
+export default App;
